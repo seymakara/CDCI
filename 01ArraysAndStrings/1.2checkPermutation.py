0357e5dd5f4758ba55d5ec2bf9a5dedee4b2caa4
seymakara <seymakara88@gmail.com>
2018-03-22 15:28:44 -0700

    added the solution with quicksort

--- 01ArraysAndStrings/1.2checkPermutation.py
+++ 01ArraysAndStrings/1.2checkPermutation.py
@@ -1,4 +1,5 @@
 #PSUEDOCODE
+#solution with hashmap
 # 1. create an empty dictionary
 # 2. check if length of strings are equal, if not return false
 # 3. for each element in string1, put them into dictionary and make the value 1, if exists ++
@@ -19,6 +20,7 @@ def checkPermuataion(input1, input2):
     for i in input2:
         if i in newDict:
             newDict[i] -=1
+            print newDict[i]
         else:
             return False
     
@@ -28,4 +30,32 @@ def checkPermuataion(input1, input2):
     return True
 
 
-print checkPermuataion("water", "watrr")
+print checkPermuataion("water", "water")
+
+#------------------------------------------------
+#solution with quicksort
+
+def quicksort(arr):
+    if len(arr)<2:
+        return arr
+    else:
+        pivot = arr[len(arr)/2]
+        less = [i for i in arr if i < pivot]
+        greater = [i for i in arr if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+def checkPermuataionQuickSort(input1, input2):
+
+    if len(input1) != len(input2):
+        return False
+    else:
+        list1 = list(input1)
+        list2 = list(input2)
+        quicksort(list1)
+        quicksort(list2)
+        if list1 == list2:
+            return True
+        else:
+            return False
+
+print checkPermuataionQuickSort("water", "water")
