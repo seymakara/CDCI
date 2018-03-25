28f706d6ef86b15dbb93f40687f0817cf9056d59
seymakara <seymakara88@gmail.com>
2018-03-25 16:31:01 -0700

    added explanations

--- 01ArraysAndStrings/1.7rotateMatrix.py
+++ 01ArraysAndStrings/1.7rotateMatrix.py
@@ -1,3 +1,4 @@
+#Matrix is n x n
 def rotateMatrix(matrix):
     newArr = [[0]*3 for i in range(3)]
 
@@ -8,3 +9,18 @@ def rotateMatrix(matrix):
     return newArr
 
 print rotateMatrix([[1,2,3], [4,5,6], [7,8,9]])
+
+#  a       b
+#(matrix) (newArr)
+# 123 ==> 741
+# 456 ==> 852
+# 789 ==> 963
+
+# a[0][0] => b[0][2]
+# a[0][1] => b[1][2]
+# a[0][2] => b[2][2]
+
+# a[0][0] => b[0][2]
+# a[0][1] => b[1][2]
+# a[0][2] => b[2][2]
+
