e2cb36809ebed561bac475753de8c555b0bcc747
seymakara <seymakara88@gmail.com>
2018-04-13 15:18:38 -0700

    modified for negative numbers and integers for 32-bit range

--- 01ArraysAndStrings/LCreverseInteger.py
+++ 01ArraysAndStrings/LCreverseInteger.py
@@ -1,15 +1,31 @@
-def reverseInteger(num):
-    reverse = 0
-    while num > 0:
-        reminder = num % 10
-        reverse = reverse * 10 + reminder
-        num = num//10
-    return reverse
+def reverseInteger(x):
+    reverse = 0 
+    if x>0:
+        while x != 0:
+            reminder = x % 10
+            reverse = reverse * 10 + reminder
+            x = x // 10
+        if reverse > pow(2,31):
+            return 0
+        else:
+            return reverse
+    elif x<0:
+        x = -1*x
+        while x != 0:
+            reminder = x % 10
+            reverse = reverse * 10 + reminder
+            x = x // 10
+        if reverse > pow(2,31):
+            return 0
+        else:
+            return -1 * reverse
+    else:
+        return 0
 
-print reverseInteger(36485)
+print reverseInteger(-36485)
 
-#hacky way
-def reverseInteger2(num):
-    return int(str(num)[::-1])
+# #hacky way
+# def reverseInteger2(num):
+#     return int(str(num)[::-1])
 
-print reverseInteger2(47849)
+# print reverseInteger2(47849)
