e2b3223838980813ae1b671b6523b84cbf24c8e8
seymakara <seymakara88@gmail.com>
2018-04-25 16:14:48 -0700

    added the definition of the question

--- 01ArraysAndStrings/1.4palindromePermutation.py
+++ 01ArraysAndStrings/1.4palindromePermutation.py
@@ -1,3 +1,6 @@
+# Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards.
+# A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+
 # 1. put them in a hash map
 # 2. increment i for duplicates
 # 3. loop through the hashmap
