920683cdf23cf4baaf374b7fa0001ce82a652c1c
seymakara <seymakara88@gmail.com>
2018-04-30 19:38:48 -0700

    first commit, contains duplicate

---
+++ 01ArraysAndStrings/LCcontainsDuplicate.py
@@ -0,0 +1,9 @@
+def reverseList(self, head):
+        return self._reverse(head)
+
+    def _reverse(self, node, prev=None):
+        if not node:
+            return prev
+        n = node.next
+        node.next = prev
+        return self._reverse(n, node)
