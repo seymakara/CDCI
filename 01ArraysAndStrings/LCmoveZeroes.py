db04e5d29d4474fce910a96e0bc675781df89dc4
seymakara <seymakara88@gmail.com>
2018-05-02 15:44:14 -0700

    first commit, move zeroes to the end of the list

---
+++ 01ArraysAndStrings/LCmoveZeroes.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        index_of_0 = 0
+        for i in range(len(nums)):
+            if nums[i]!= 0:
+                nums[index_of_0], nums[i] = nums[i], nums[index_of_0] #burda i ve index_of_0 ayni olcagi icin aslinda swap etmiyor.
+                index_of_0 += 1
