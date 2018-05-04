be0aa27c49d41ba355ed8e9cfb8feaf861ab0bb3
seymakara <seymakara88@gmail.com>
2018-05-03 20:32:31 -0700

    changed pythonic swap into swap algorithm

--- 01ArraysAndStrings/LCmoveZeroes.py
+++ 01ArraysAndStrings/LCmoveZeroes.py
@@ -3,5 +3,8 @@ class Solution(object):
         index_of_0 = 0
         for i in range(len(nums)):
             if nums[i]!= 0:
-                nums[index_of_0], nums[i] = nums[i], nums[index_of_0] #burda i ve index_of_0 ayni olcagi icin aslinda swap etmiyor.
+                # nums[index_of_0], nums[i] = nums[i], nums[index_of_0] #burda i ve index_of_0 ayni olcagi icin aslinda swap etmiyor. pythonic swap
+                temp = nums[index_of_0] # swap algorithm 
+                nums[index_of_0] = nums[i]
+                nums[i] = temp
                 index_of_0 += 1
