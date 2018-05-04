d9a4dbb19f136f655057aa85c1e4bb0a3effdd26
seymakara <seymakara88@gmail.com>
2018-05-03 21:42:56 -0700

    refactored

--- 01ArraysAndStrings/LCmoveZeroes.py
+++ 01ArraysAndStrings/LCmoveZeroes.py
@@ -3,7 +3,7 @@ class Solution(object):
         index_of_0 = 0
         for i in range(len(nums)):
             if nums[i]!= 0:
-                # nums[index_of_0], nums[i] = nums[i], nums[index_of_0] #burda i ve index_of_0 ayni olcagi icin aslinda swap etmiyor. pythonic swap
+                # nums[index_of_0], nums[i] = nums[i], nums[index_of_0] #burda i ve index_of_0 ayni olcagi icin aslinda swap etmiyor. this is a pythonic swap
                 temp = nums[index_of_0] # swap algorithm 
                 nums[index_of_0] = nums[i]
                 nums[i] = temp
