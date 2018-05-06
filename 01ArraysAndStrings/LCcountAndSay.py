9dfa25dd6384a2e4bba6cd6fc6f1ff9eff8a082a
seymakara <seymakara88@gmail.com>
2018-05-05 23:56:03 -0700

    first commit, count and say

---
+++ 01ArraysAndStrings/LCcountAndSay.py
@@ -0,0 +1,23 @@
+# The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+# 1.     1
+# 2.     11
+# 3.     21
+# 4.     1211
+# 5.     111221
+
+class Solution(object):
+    def countAndSay(self, n):
+        s = '1'
+        for _ in range(n-1):
+            let, temp, count = s[0], '', 0
+            for l in s:
+                if let == l:
+                    count += 1
+                else:
+                    temp += str(count)+let
+                    let = l
+                    count = 1
+            temp += str(count)+let
+            s = temp
+        return s
