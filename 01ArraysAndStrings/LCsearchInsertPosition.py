f150878f56f42baff4d5b2cf2688261c22ce2b82
seymakara <seymakara88@gmail.com>
2018-05-08 16:00:44 -0700

    added binary search solution

--- 01ArraysAndStrings/LCsearchInsertPosition.py
+++ 01ArraysAndStrings/LCsearchInsertPosition.py
@@ -5,8 +5,16 @@
 class Solution(object):
     def searchInsert(self, nums, target):
         return len([x for x in nums if x<target])
-        index = 0
-        for num in nums:
-            if num <= target:
-                index += 1
-        return index
+    
+    def searchInsert2(self, nums, target):
+        start = 0
+        end = len(nums)-1
+        while start <= end :
+            mid  = (start + end) // 2
+            if nums[mid]  == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid -1
+        return start
