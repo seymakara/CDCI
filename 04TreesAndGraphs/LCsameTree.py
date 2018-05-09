81f2c5efea3dde2ebaec10862927d58b633e97cf
seymakara <seymakara88@gmail.com>
2018-05-08 18:32:04 -0700

    first commit, same tree

---
+++ 04TreesAndGraphs/LCsameTree.py
@@ -0,0 +1,19 @@
+# Given two binary trees, write a function to check if they are the same or not.
+# Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return False
