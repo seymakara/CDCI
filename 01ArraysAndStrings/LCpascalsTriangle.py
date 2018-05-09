016706fa8d10a0ded9d8a07f15c984296ee00a51
seymakara <seymakara88@gmail.com>
2018-05-09 15:17:57 -0700

    added explanation

--- 01ArraysAndStrings/LCpascalsTriangle.py
+++ 01ArraysAndStrings/LCpascalsTriangle.py
@@ -1,9 +1,20 @@
+# Example:
+
+# Input: 5
+# Output:
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
 class Solution(object):
     def generate(self, numRows):
         listOfRows = []
         for i in range(numRows):
-            listOfRows.append([1]*(i+1)) #creastes list of rows including the items as many as the number of the row
+            listOfRows.append([1]*(i+1)) #creastes the list of rows including the items as many as the number of the row
             if i>1: #start from the third row. You don't need to change anything for the first two lines.
                 for j in range(1,i): # the first and the last items are 1. So exclude them.
-                    listOfRows[i][j]=listOfRows[i-1][j-1]+listOfRows[i-1][j] # the some of an item equals to sum of the item at the same index and the previous item in the previous row.
+                    listOfRows[i][j]=listOfRows[i-1][j-1]+listOfRows[i-1][j] # bir rakamin toplami ondan onceki siradaki onla ayni hizadaki ve ondan bi onceki hizadaki rakamin toplamina esittir..
         return listOfRows
