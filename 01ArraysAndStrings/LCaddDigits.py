e5eedde605f39ef608662b6f3ee0ab7a1051f0fe
seymakara <seymakara88@gmail.com>
2018-05-16 16:40:31 -0700

    first commit, add digits

---
+++ 01ArraysAndStrings/LCaddDigits.py
@@ -0,0 +1,18 @@
+# Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+# Example:
+
+# Input: 38
+# Output: 2 
+# Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+#              Since 2 has only one digit, return it.
+
+class Solution(object):
+    def addDigits(self, num):
+        while num >= 10:
+            temp = 0
+            while num > 0:
+                temp += num % 10
+                num /= 10
+            num = temp
+        return num
